2e149f3b3deddf2982ae34bccbd75609384727b4
Emerson Seiler <seiler.emerson@gmail.com>
2022-10-19 11:43:59 -0300

    Create command.sql

---
+++ command.sql
@@ -0,0 +1,7 @@
+select * from patient_emr;
+select * from doctor_emr;
+select * from user_emr;
+select * from team_emr;
+select * from appointment_emr;
+
+update appointment_emr set date_appointment = "2022-10-06 12:00:00" where id = 2;
